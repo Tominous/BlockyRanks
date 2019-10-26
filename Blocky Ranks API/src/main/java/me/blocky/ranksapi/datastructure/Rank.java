b11cf45b7b6bdf2c02c731f7d2386c352c7514cc
Martin Borzan <martin.borzan@yahoo.com>
2019-10-26 18:13:55 +0200

    Minecraft 1.15 compatibility update

--- Blocky Ranks API/src/main/java/me/blocky/ranksapi/datastructure/Rank.java
+++ Blocky Ranks API/src/main/java/me/blocky/ranksapi/datastructure/Rank.java
@@ -1,23 +1,24 @@
 package me.blocky.ranksapi.datastructure;
 
 import java.util.List;
+
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 
 import me.blocky.ranksapi.requirement.Requirement;
 
-public abstract class Rank {  
+public abstract class Rank {
   /**
    * Gets the rank identifier of this rank.
    */
   public abstract String getIdentifier();
 
   /**
-   * Gets the name of the rank as displayed to the user in the gui. 
+   * Gets the name of the rank as displayed to the user in the gui.
    * Can contain ChatColor color codes.
    */
   public abstract String getDisplayName();
-  
+
   /**
    * Gets the list of requirements needed to rank up to this rank.
    */
@@ -43,9 +44,9 @@ public abstract class Rank {
    */
   @Deprecated
   public abstract Material getMaterial();
-  
+
   /**
    * Gets a clone of the item that represents this rank in the gui.
    */
-  public abstract ItemStack getItemStack(); 
+  public abstract ItemStack getItemStack();
 }
